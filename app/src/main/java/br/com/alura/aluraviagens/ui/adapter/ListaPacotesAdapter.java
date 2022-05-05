0679108f89ec09375bd5514c09223c189ac0f30f
akiira23 <aah950@hotmail.com>
2022-05-05 17:10:34 -0300

    Formatando as informações

--- app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -10,7 +10,11 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
@@ -57,10 +61,24 @@ public class ListaPacotesAdapter extends BaseAdapter {
         imagem.setImageDrawable(drawableImagemPacote);
 
         TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
-        dias.setText(pacote.getDias() + " dias");
+        int qtdDeDias = pacote.getDias();
+        String diasEmTexto = "";
+
+        if (qtdDeDias > 1)
+            diasEmTexto = qtdDeDias + " dias";
+        else
+            diasEmTexto = qtdDeDias + " dia";
+
+        dias.setText(diasEmTexto);
 
         TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
-        preco.setText("R$ " + pacote.getPreco().toString());
+        NumberFormat formatoBrasileiro = DecimalFormat.getCurrencyInstance(
+                new Locale("pt", "br"));
+        BigDecimal precoDoPacote = pacote.getPreco();
+        String moedaBrasileira = formatoBrasileiro
+                .format(precoDoPacote)
+                .replace("R$", "R$ ");
+        preco.setText(moedaBrasileira);
 
         return viewCriada;
     }
